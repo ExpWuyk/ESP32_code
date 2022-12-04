a8569d8d20633434c6204b01710835460b663047
MagicWyk <1623606528@qq.com>
2022-12-04 23:20:18 +0800

    WiFi开关灯测试

--- ESP32-S3-w/ESP32-S3-w/src/main.cpp
+++ ESP32-S3-w/ESP32-S3-w/src/main.cpp
@@ -1,29 +1,76 @@
+/*
+
+
+*/
+
+
+#define BLINKER_WIFI
+
+#include <Blinker.h>
+
 #include <Arduino.h>
 #include <FastLED.h>
 
-
-#define LED_PIN  48        //定义LED引脚
 #define NUM_LEDS 1         //RGB灯个数
 CRGB leds[NUM_LEDS];
-uint8_t max_bright = 10;  // LED亮度控制变量，可使用数值为 0 ～ 255， 数值越大则光带亮度越高
+
+char auth[] = "517931d32d0f";
+char ssid[] = "W";
+char pswd[] = "12345678";
+
+// 新建组件对象
+BlinkerButton Button1("btn-on");
+BlinkerButton Button2("btn-off");
+BlinkerNumber Number1("num-abc");
 
 
-void setup() 
-{  
-  Serial.begin(115200);
+int counter = 0;
 
-  FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS); // 初始化光带
-  FastLED.setBrightness(max_bright);                     // 设置光带亮度
-  // put your setup code here, to run once:
-}
-
-void loop() 
+// 按下按键即会执行该函数
+void button1_callback(const String & state)
 {
-  
-  Serial.println("huise");           //串口打印灯的颜色
-  leds[0] = CRGB::Green;              //CRGB ( 122, 115, 116);  由三原色  红 绿 蓝 调节不同亮度改变灯的颜色 
-  FastLED.show();                    // 更新LED色彩
-  delay(1000);
- 
- 
+    BLINKER_LOG("get button state: ", state);
+    //digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    leds[0] = CRGB::Green;              //CRGB ( 122, 115, 116);  由三原色  红 绿 蓝 调节不同亮度改变灯的颜色 
+    FastLED.show();
+    
 }
+
+void button2_callback(const String & state)
+{
+    BLINKER_LOG("get button state: ", state);
+    FastLED.clear();
+    FastLED.show();
+
+}
+
+// 如果未绑定的组件被触发，则会执行其中内容
+void dataRead(const String & data)
+{
+    BLINKER_LOG("Blinker readString: ", data);
+    counter++;
+    Number1.print(counter);
+}
+
+void setup()
+{
+    // 初始化串口
+    Serial.begin(115200);
+    BLINKER_DEBUG.stream(Serial);
+    BLINKER_DEBUG.debugAll();
+
+    // 初始化有LED的IO
+    FastLED.addLeds<WS2812, 48, GRB>(leds, NUM_LEDS); // 初始化光带
+
+    // 初始化blinker
+    Blinker.begin(auth, ssid, pswd);
+    Blinker.attachData(dataRead);
+
+    Button1.attach(button1_callback);
+    Button2.attach(button2_callback);
+}
+
+void loop() {
+    Blinker.run();
+}
+
